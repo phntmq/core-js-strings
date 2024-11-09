bd7be3922818612dc66c896527de650a41197565
phntmq <Ax3113r@gmail.com>
2024-11-09 15:00:47 +0800

    feat: solve findLongestWord

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -371,8 +371,10 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence
+    .split(' ')
+    .reduce((acc, word) => (acc.length < word.length ? word : acc), '');
 }
 
 /**
