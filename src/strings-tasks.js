b374bb41d2a73cfa082d31815ff55a36b1a1c5a2
phntmq <Ax3113r@gmail.com>
2024-11-09 14:01:57 +0800

    feat: solve countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -305,8 +305,9 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  return [...str.toLowerCase()].filter((char) => vowels.includes(char)).length;
 }
 
 /**
